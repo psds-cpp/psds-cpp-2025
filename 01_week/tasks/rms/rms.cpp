973e2c164cef129cd09ba927fff2d4ad8405222f
EugeniusMiroshnichenko <zhnya.miroshnichenko@mail.ru>
2025-11-23 18:04:40 +0500

    RMS done

--- 01_week/tasks/rms/rms.cpp
+++ 01_week/tasks/rms/rms.cpp
@@ -1,7 +1,12 @@
 #include <cstddef>
-#include <stdexcept>
+#include <cmath>
 
 
 double CalculateRMS(double values[], size_t size) {
-    throw std::runtime_error{"Not implemented"};
+    double res = 0.0;
+    if (values == nullptr || size == 0) return res;
+    for (size_t i=0; i < size; ++i){
+        res += values[i] * values[i];
+    }
+    return std::sqrt(res / size);
 }
