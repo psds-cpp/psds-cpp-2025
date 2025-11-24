f299b480d8e91f7169fce6c4f0a9ed8439e18192
Eremin Daniil <danchchik@bk.ru>
2025-11-24 10:56:55 +0500

    add solution: add addition task

--- 01_week/tasks/addition/addition.cpp
+++ 01_week/tasks/addition/addition.cpp
@@ -3,6 +3,6 @@
 
 
 int64_t Addition(int a, int b) {
-    int64_t res = static_cast<int64_t>(a) + b;
+    int64_t res = static_cast<int64_t>(a) + b;  //явно преобразуем одно из слагаемых в int64_t
     return res;
 }
