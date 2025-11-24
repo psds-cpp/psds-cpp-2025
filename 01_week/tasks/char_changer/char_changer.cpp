4141f23ff6a9c850cd2933433e37ee5110dec93c
Radiy <notron12460@gmail.com>
2025-11-24 18:26:38 +0500

    wip (solution): wip char_changer task

--- 01_week/tasks/char_changer/char_changer.cpp
+++ 01_week/tasks/char_changer/char_changer.cpp
@@ -3,5 +3,14 @@
 
 
 size_t CharChanger(char array[], size_t size, char delimiter = ' ') {
+    size_t convertedSymbol = 0; currentSybmol
+
+    for (size_t i = 0; i < size; ++i) {    
+        if (!isalpha(array[])) {
+            
+        }
+    }
+
+
     throw std::runtime_error{"Not implemented"};
 }
