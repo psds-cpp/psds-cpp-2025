c7622edba8acd1452f35c67d1450fe8152f354b4
Polina785643 <polytutynina@gmail.com>
2025-11-26 00:04:49 +0500

    Написана функция CharChanger

--- 01_week/tasks/char_changer/char_changer.cpp
+++ 01_week/tasks/char_changer/char_changer.cpp
@@ -1,7 +1,100 @@
 #include <cstddef>
-#include <stdexcept>
+#include <cctype>
+
+/* Преобразует строку по правилам:
+ - Повторяющиеся символы (кроме пробелов) заменяются на символ+число повторений
+ - Цифры заменяются на '*'
+ - Заглавные латинские буквы преобразуются в строчные  
+ - Последовательные пробелы заменяются на разделитель
+ - Остальные символы заменяются на '_'
+ Возвращает новую длину строки */
 
 
-size_t CharChanger(char array[], size_t size, char delimiter = ' ') {
-    throw std::runtime_error{"Not implemented"};
+size_t CharChanger(char array[], size_t size, char delimiter) {
+    if (size == 0) return 0; //проверка на нулевую строку
+    
+    size_t write_pos = 0;  //позиция записи в результирующий массив 
+    char prev_char = '\0'; //предыдущий преобразованный символ
+    size_t count = 1;      //счетчик повторений текущего символа
+    
+    // Цикл по строке пока не встретим нуль-терминатор
+    for (size_t read_pos = 0; read_pos < size && array[read_pos] != '\0'; read_pos++) {
+        char current_char = array[read_pos];
+        
+        // Преобразуем символ по правилам
+        char transformed_char = current_char;
+        
+        // Цифры -> '*'
+        if (std::isdigit(current_char)) { 
+            transformed_char = '*';
+        } 
+        // Заглавные буквы -> строчные
+        else if (std::isupper(current_char)) { 
+            transformed_char = std::tolower(current_char);
+        } 
+        // Остальные символы -> '_' (кроме пробелов и строчных букв)
+        else if (current_char != ' ' && !std::islower(current_char)) { //
+            transformed_char = '_';
+        }
+        
+        // Обработка пробелов
+        if (current_char == ' ') {
+            // Добавляем разделитель только если он не дублируется
+            if (write_pos == 0 || array[write_pos - 1] != delimiter) {
+                array[write_pos++] = delimiter;
+            }
+
+            // Сброс состояния последовательности
+            prev_char = '\0';
+            continue;
+        }
+        
+        // Проверяем, продолжается ли последовательность
+        if (transformed_char == prev_char) {
+            count++;
+        } else {
+            // Записываем предыдущую последовательность
+            if (prev_char != '\0') {
+
+                //Записываем сам символ
+                array[write_pos++] = prev_char;
+
+                //Записываем число повторений
+                if (count > 1) {
+                    if (count >= 10) {
+                        array[write_pos++] = '0';
+                    } else {
+                        array[write_pos++] = '0' + count;
+                    }
+                }
+            }
+            // Начинаем новую последовательность
+            prev_char = transformed_char;
+            count = 1;
+        }
+    }
+    
+    // Записываем последнюю последовательность
+    if (prev_char != '\0') {
+        array[write_pos++] = prev_char;
+        if (count > 1) {
+            if (count >= 10) {
+                array[write_pos++] = '0';
+            } else {
+                array[write_pos++] = '0' + count;
+            }
+        }
+    }
+
+    // Проверка, что строка правильно завершена нуль-терминатором
+    if (write_pos < size) {
+        array[write_pos] = '\0';
+    }
+
+    // Если буфер меньше чем строка
+    else if (size > 0) {
+        array[size - 1] = '\0';
+    }
+    
+    return write_pos;
 }
