057cea08900b18fc14fa4817032f33437ac647df
Radiy <notron12460@gmail.com>
2025-11-27 14:39:30 +0500

    add (solution): add print_bits task

--- 01_week/tasks/print_bits/print_bits.cpp
+++ 01_week/tasks/print_bits/print_bits.cpp
@@ -1,7 +1,17 @@
 #include <cstddef>
 #include <stdexcept>
-
+#include <string>
 
 void PrintBits(long long value, size_t bytes) {
-    throw std::runtime_error{"Not implemented"};
+    std::string byteForm = "0b";
+    // i < bytes * 8 сработает, потому что 0ULL - 1 переполняется в ULL_MAX
+    for (size_t i = (bytes * 8 - 1); i < bytes * 8; --i) {
+       byteForm += ((value >> i) & 0x01U) + '0';
+
+       if (i != 0 && i % 4 == 0) {
+            byteForm += "'";    
+       }
+    }
+
+    std::cout << byteForm << std::endl;
 }
