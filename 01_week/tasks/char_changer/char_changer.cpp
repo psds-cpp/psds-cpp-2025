b26dd571b43536e3b0520059cf7624d66fa17562
Polina785643 <polytutynina@gmail.com>
2025-11-28 01:29:26 +0500

    Правки

--- 01_week/tasks/char_changer/char_changer.cpp
+++ 01_week/tasks/char_changer/char_changer.cpp
@@ -4,63 +4,53 @@
 /* Преобразует строку по правилам:
  - Повторяющиеся символы (кроме пробелов) заменяются на символ+число повторений
  - Цифры заменяются на '*'
- - Строчные латинские буквы не изменяются
- - Прописные латинские буквы заменяются на строчные
+ - Строчные латинские буквы преобразуются в прописные
+ - Прописные латинские буквы остаются без изменений
  - Последовательные пробелы заменяются на разделитель
  - Остальные символы заменяются на '_'
  Возвращает новую длину строки */
 
 size_t CharChanger(char array[], size_t size, char delimiter) {
-    if (size == 0) return 0; // Проверка на нулевую строку
+    if (size == 0) return 0;
     
-    size_t write_pos = 0;  // Позиция записи в результирующий массив 
-    char prev_char = '\0'; // Предыдущий преобразованный символ
-    size_t count = 1;      // Счетчик повторений текущего символа
+    size_t write_pos = 0;
+    char prev_char = '\0';
+    size_t count = 1;
     
-    // Цикл по строке пока не встретим нуль-терминатор
     for (size_t read_pos = 0; read_pos < size && array[read_pos] != '\0'; read_pos++) {
         char current_char = array[read_pos];
-        
-        // Преобразуем символ по правилам
         char transformed_char = current_char;
         
         // Цифры -> '*'
-        if (std::isdigit(current_char)) { 
+        if (std::isdigit(current_char)) {
             transformed_char = '*';
-        } 
-        // Прописные буквы -> строчные
-        else if (std::isupper(current_char)) { 
-            transformed_char = std::tolower(current_char);
         }
-        // Строчные латинские буквы остаются без изменений
+        // Строчные латинские -> прописные
+        else if (std::islower(current_char)) {
+            transformed_char = std::toupper(current_char);
+        }
         // Остальные символы -> '_' (кроме пробелов и латинских букв)
-        else if (current_char != ' ' && !std::islower(current_char) && !std::isupper(current_char)) {
+        else if (current_char != ' ' && !std::isupper(current_char) && !std::islower(current_char)) {
             transformed_char = '_';
         }
         
-        // Обработка пробелов
+        // Пробелы -> разделитель
         if (current_char == ' ') {
-            // Добавляем разделитель только если он не дублируется
             if (write_pos == 0 || array[write_pos - 1] != delimiter) {
                 array[write_pos++] = delimiter;
             }
-
-            // Сброс состояния последовательности
             prev_char = '\0';
             count = 1;
             continue;
         }
         
-        // Проверяем, продолжается ли последовательность
+        // Обработка последовательностей символов
         if (transformed_char == prev_char) {
             count++;
         } else {
-            // Записываем предыдущую последовательность
+            // Запись предыдущей последовательности
             if (prev_char != '\0') {
-                // Записываем сам символ
                 array[write_pos++] = prev_char;
-
-                // Записываем число повторений
                 if (count > 1) {
                     if (count >= 10) {
                         array[write_pos++] = '0';
@@ -69,13 +59,13 @@ size_t CharChanger(char array[], size_t size, char delimiter) {
                     }
                 }
             }
-            // Начинаем новую последовательность
+            // Начало новой последовательности
             prev_char = transformed_char;
             count = 1;
         }
     }
     
-    // Записываем последнюю последовательность
+    // Запись последней последовательности
     if (prev_char != '\0') {
         array[write_pos++] = prev_char;
         if (count > 1) {
@@ -86,13 +76,11 @@ size_t CharChanger(char array[], size_t size, char delimiter) {
             }
         }
     }
-
-    // Проверка того, что строка правильно завершена нуль-терминатором
+    
+    // Завершение строки
     if (write_pos < size) {
         array[write_pos] = '\0';
-    }
-    // Если буфер меньше чем строка
-    else if (size > 0) {
+    } else if (size > 0) {
         array[size - 1] = '\0';
     }
     
