15175f36344d81d1eb3375a28741fd64f1368c59
karskanovas <yuktyui47@gmail.com>
2025-11-28 22:01:25 +0500

    fix: add char_changer task

--- 01_week/tasks/char_changer/char_changer.cpp
+++ 01_week/tasks/char_changer/char_changer.cpp
@@ -3,54 +3,43 @@
 #include <cstring>
 
 size_t CharChanger(char array[], size_t size, char delimiter = ' ') {
-    size_t write_idx = 0;
-    auto get_mapped_char = [&](char ch) -> char {
-        if (std::isspace(static_cast<unsigned char>(ch))) {
-            return delimiter;
-        } else if (std::isdigit(static_cast<unsigned char>(ch))) {
-            return '*';
-        } else if (std::isalpha(static_cast<unsigned char>(ch))) {
-            return std::toupper(static_cast<unsigned char>(ch));
-        } else {
-            return '_';}
-    };
-    size_t read_idx = 0;
-    while (read_idx < size && array[read_idx] != '\0') {
-        char current_char = array[read_idx];
-        char mapped_char = get_mapped_char(current_char);
+    size_t w = 0; 
+    auto map_char = [&](char ch) -> char {
+        unsigned char uc = ch; //в этом месте возникала ошибка, решено использовать unsigner
+        if (std::isspace(uc)) return delimiter;
+        if (std::isdigit(uc)) return '*';
+        if (std::isalpha(uc)) return (char)std::toupper(uc);
+        return '_'; };
+    size_t r = 0; 
+    while (r < size && array[r] != '\0') {
+        char current = array[r];
+        char mapped = map_char(current);
         size_t count = 1;
-        size_t next_idx = read_idx + 1;
-        while (next_idx < size && array[next_idx] != '\0') {
-            char next_char = array[next_idx];
-            char mapped_next_char = get_mapped_char(next_char);
-            if (mapped_next_char != mapped_char) break;
-            if (mapped_char == '*' || mapped_char == '_') {
-                if (current_char != next_char) break; 
-            }
-            if (mapped_char >= 'A' && mapped_char <= 'Z') {
-                bool current_is_lower = std::islower(static_cast<unsigned char>(current_char));
-                bool next_is_lower = std::islower(static_cast<unsigned char>(next_char));
-                
-                if (current_is_lower != next_is_lower) break;
-            }
+        size_t next = r + 1;
+        while (next < size && array[next] != '\0') {
+            char next_char = array[next];
+            char mapped_next = map_char(next_char);           
+            if (mapped_next != mapped) break;
+            if (mapped == '*' || mapped == '_') {
+                if (current != next_char) break; 
+            } else if (mapped >= 'A' && mapped <= 'Z') {
+                unsigned char curr_uc = current;
+                unsigned char next_uc = next_char;
+                if (std::islower(curr_uc) != std::islower(next_uc)) break; }
             count++;
-            next_idx++;}
-        bool is_delimiter_sequence = (mapped_char == delimiter) && std::isspace(static_cast<unsigned char>(current_char));
-        if (is_delimiter_sequence) {
-            if (write_idx == 0 || array[write_idx - 1] != delimiter) {
-                array[write_idx++] = delimiter;
-            }
-        } else {
-            array[write_idx++] = mapped_char;
-
+            next++;}
+        unsigned char current_uc = current;
+        bool is_space_seq = (mapped == delimiter) && std::isspace(current_uc);   
+        if (is_space_seq) { 
+            bool is_letter_delimiter = (delimiter >= 'A' && delimiter <= 'Z'); 
+            if (is_letter_delimiter || w == 0 || array[w - 1] != delimiter) {
+                array[w++] = delimiter;}
+        } else { 
+            array[w++] = mapped;
             if (count > 1) {
-                if (count < 10) {
-                    array[write_idx++] = '0' + count;
-                } else {
-                    array[write_idx++] = '0';}
-            }
-        }
-        read_idx += count;}
-    array[write_idx] = '\0';
-    return write_idx;
+                char repeat_char = (count < 10) ? ('0' + count) : '0';
+                array[w++] = repeat_char;}}
+        r += count; }
+    array[w] = '\0'; 
+    return w;
 }
