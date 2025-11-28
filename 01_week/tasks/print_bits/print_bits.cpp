a3f7150e19c00c2ac6f96f530cb405ea192919ff
KaigorodovMM <kai-tram-pam-pam@mail.ru>
2025-11-28 22:38:42 +0500

    add solution: add print_bits task

--- 01_week/tasks/print_bits/print_bits.cpp
+++ 01_week/tasks/print_bits/print_bits.cpp
@@ -1,7 +1,17 @@
 #include <cstddef>
 #include <stdexcept>
+#include <iostream>
 
 
 void PrintBits(long long value, size_t bytes) {
-    throw std::runtime_error{"Not implemented"};
+    if (bytes > 0 && bytes <= 8){
+        std::cout << "0b";
+        for(size_t i = 0; i < bytes * 8; ++i) {
+            std::cout << ((value >> (bytes * 8 - i - 1)) & 1);
+            if (i != (bytes * 8 - 1) && (i % 4 == 3) && i != 0)
+                std::cout << '\'';
+            else if (i == bytes * 8 - 1)
+                std::cout << '\n';
+        }
+    }
 }
