dad85a7a2665ce8a4537901e9e329764a0568edd
vova22013 <104551154+vova22013@users.noreply.github.com>
2025-11-28 16:36:49 +0000

    Update addition.cpp

--- 01_week/tasks/addition/addition.cpp
+++ 01_week/tasks/addition/addition.cpp
@@ -1,7 +1,8 @@
 #include <cstdint>
 #include <stdexcept>
 
-
 int64_t Addition(int a, int b) {
-    throw std::runtime_error{"Not implemented"};
+    int64_t a_64 = static_cast<int64_t>(a);
+    int64_t b_64 = static_cast<int64_t>(b);
+    return a_64 + b_64;
 }
