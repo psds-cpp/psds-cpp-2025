b5703271f6e16ef3cc4a297a34f438a24e19b6d6
karskanovas <yuktyui47@gmail.com>
2025-11-28 21:04:25 +0500

    add: add rms task

--- 01_week/tasks/rms/rms.cpp
+++ 01_week/tasks/rms/rms.cpp
@@ -1,7 +1,12 @@
-#include <cstdef>
+#include <cstddef>  
+#include <cmath>    
 #include <stdexcept>
 
-
 double CalculateRMS(double values[], size_t size) {
-    throw std::runtime_error{"Not implemented"};
+    if (size == 0) return 0.0;
+    if (values == nullptr) return 0.0;  
+    double sum_squares = 0.0;
+    for (size_t i = 0; i < size; ++i) {
+        sum_squares += values[i] * values[i];}
+    return std::sqrt(sum_squares / static_cast<double>(size));
 }
