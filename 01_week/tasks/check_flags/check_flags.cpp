6ba1cc784a1c4cc48ebff52908a1aa246e069aab
Lenar Gatin <lgatin1711@gmail.com>
2025-11-28 21:24:24 +0500

    add (solution): add check_flags task

--- 01_week/tasks/check_flags/check_flags.cpp
+++ 01_week/tasks/check_flags/check_flags.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
-#include <stdexcept>
+#include <string>
+#include <vector>
+#include <iostream>
 
 
 enum class CheckFlags : uint8_t {
@@ -13,6 +15,57 @@ enum class CheckFlags : uint8_t {
     ALL = TIME | DATE | USER | CERT | KEYS | DEST
 };
 
+CheckFlags operator&(CheckFlags lhs, CheckFlags rhs) {
+    return static_cast<CheckFlags>(static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs));
+}
+
+std::string GetFlagName(CheckFlags flags) {
+    switch (flags) {
+        case CheckFlags::TIME:
+            return "TIME";
+        case CheckFlags::DATE:
+            return "DATE";
+        case CheckFlags::USER:
+            return "USER";
+        case CheckFlags::CERT:
+            return "CERT";
+        case CheckFlags::KEYS:
+            return "KEYS";
+        case CheckFlags::DEST:
+            return "DEST";
+        default:
+            return "";
+    }
+}
+
 void PrintCheckFlags(CheckFlags flags) {
-    throw std::runtime_error{"Not implemented"};
+    // Проверка на выход за диапозон значений CheckFlags
+    if (static_cast<int>(CheckFlags::ALL) < static_cast<int>(flags)) {
+        return;
+    }
+
+    std::vector<CheckFlags> checks{
+        CheckFlags::TIME, 
+        CheckFlags::DATE,
+        CheckFlags::USER,
+        CheckFlags::CERT,
+        CheckFlags::KEYS,
+        CheckFlags::DEST,
+    };
+
+    std::vector<std::string> flags_names{};
+    for (auto &check : checks) {
+        if (std::string flag_name = GetFlagName(flags & check); flag_name != "") {
+            flags_names.push_back(flag_name);
+        }
+    }
+
+    std::string result = "[";
+    for (size_t i = 0, flags_size = flags_names.size(); i < flags_size; ++i) {
+        result += flags_names[i];
+        if (i != flags_size - 1) {
+            result += ',';
+        }
+    }
+    std::cout << result << ']';
 }
