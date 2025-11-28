73267dff7fc1d2d7bb486c72871cf7067acd436d
karskanovas <yuktyui47@gmail.com>
2025-11-28 20:24:18 +0500

    add: add length_lit task

---
+++ 01_week/tasks/length_lit/length_lit.cpp
@@ -0,0 +1,49 @@
+#include <cstdint>
+
+constexpr long double operator"" _ft_to_m(long double v) {
+    return static_cast<double>(v * 0.3048L);
+}
+
+constexpr long double operator"" _ft_to_in(long double v) {
+    return static_cast<double>(v * 12.0L);
+}
+
+constexpr long double operator"" _ft_to_cm(long double v) {
+    return static_cast<double>(v * 30.48L);
+}
+
+constexpr long double operator"" _in_to_ft(long double v) {
+    return static_cast<double>(v / 12.0L);
+}
+
+constexpr long double operator"" _in_to_m(long double v) {
+    return static_cast<double>(v * 0.0254L);
+}
+
+constexpr long double operator"" _in_to_cm(long double v) {
+    return static_cast<double>(v * 2.54L);
+}
+
+constexpr long double operator"" _cm_to_in(long double v) {
+    return static_cast<double>(v / 2.54L);
+}
+
+constexpr long double operator"" _cm_to_ft(long double v) {
+    return static_cast<double>(v / 30.48L);
+}
+
+constexpr long double operator"" _cm_to_m(long double v) {
+    return static_cast<double>(v / 100.0L);
+}
+
+constexpr long double operator"" _m_to_cm(long double v) {
+    return static_cast<double>(v * 100.0L);
+}
+
+constexpr long double operator"" _m_to_in(long double v) {
+    return static_cast<double>(v / 0.0254L);
+}
+
+constexpr long double operator"" _m_to_ft(long double v) {
+    return static_cast<double>(v / 0.3048L);
+}
