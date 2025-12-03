53e1f09062e622fb5b13126ef3910cf2a749cc89
Nicolay Petrov <lilpip2002@mail.ru>
2025-12-03 18:28:25 +0500

    add (solution): add func_array1 task

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,10 +1,17 @@
 #include <stdexcept>
 
 
-double ApplyOperations(double a, double b, double (*ops[])(double,double), size_t size) {
+double ApplyOperations(double a, double b, double (**ops)(double, double), size_t size) {
+    if (ops == nullptr || size == 0) {
+        return 0.0;
+    }
+    
     double sum = 0.0;
-     for (size_t i = 0; i < size; ++i){
-        sum += ops[i](a,b);
-     }
+    for (size_t i = 0; i < size; ++i) {
+        if (ops[i] != nullptr) {
+            sum += ops[i](a, b);
+        }
+    }
+    
     return sum;
 }
