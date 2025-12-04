3ec5892c5aec9c1ebb32698e4360b32506049eff
Grigory <fess96@list.ru>
2025-12-04 22:02:26 +0500

    Add solution 2: add pretty_array task

--- 02_week/tasks/pretty_array/pretty_array.cpp
+++ 02_week/tasks/pretty_array/pretty_array.cpp
@@ -5,62 +5,38 @@
 
 void PrintArray(const int* begin, const int* end, unsigned int limiter = 0) {
     std::string numbers = "[";
-    if (begin == nullptr || end == nullptr){
+    int dist = end - begin;
+    unsigned int count = 0;
+    if (begin == nullptr || end == nullptr || (dist*dist) == 0){ // Проверка на nullptr или пустой массив
         numbers+="]";
         std::cout << numbers << std::endl;
         return;
     }
-    unsigned int i = 0; // счетчик символов для использования ограничителя длины
-    int dist = end - begin;
-    if (dist > 0) { // ЗАпись слева направо
-        while (begin < end){
-            int num = *begin;
-            std::string reverse_number = "";
-            while (num / 10 != 0) { // // Конвертация int в str и запись в массив numbers
-                reverse_number += '0' + (num % 10);
-                num /= 10;
-            }
-            reverse_number += '0' + num % 10;
-            // Переворачиваем string number
-            std::string number;
-            for (int j = reverse_number.length() - 1; j >=0; --j) {
-                number += reverse_number[j];
-            }
-            numbers += number;
-            if (end - begin == 1) break; // ПРоверка последний ли это символ?
-            numbers += ", "; // Если не последний то добавляем ", "
-            ++i; // Если счетчик равен ограничителю, то добавляем "... "
-            if (limiter != 0 && i == limiter){
-                numbers += "...\n ";
-                i = 0;
-            }
-            ++begin;
+
+    while (true){
+        int num = *begin;
+        std::string reverse_number = "";
+        while (num / 10 != 0) { // // Конвертация int в str и запись в массив numbers
+            reverse_number += '0' + (num % 10);
+            num /= 10;
         }
-    }
-    else if (dist < 0) { // ЗАпись справо налево
-        while (begin > end){
-            int num = *begin;
-            std::string reverse_number = "";
-            while (num / 10 != 0) { // // Конвертация int в str и запись в массив numbers
-                reverse_number += '0' + (num % 10);
-                num /= 10;
-            }
-            reverse_number += '0' + num % 10;
-            // Переворачиваем string number
-            std::string number;
-            for (int j = reverse_number.length() - 1; j >=0; --j) {
-                number += reverse_number[j];
-            }
-            numbers += number;
-            if (begin - end == 1) break; // ПРоверка последний ли это символ?
-            numbers += ", "; // Если не последний то добавляем ", "
-            ++i; // Если счетчик равен ограничителю, то добавляем "... "
-            if (limiter != 0 && i == limiter){
-                numbers += "...\n ";
-                i = 0;
-            }
-            --begin;
+        reverse_number += '0' + num % 10;
+
+        // Переворачиваем string number
+        std::string number;
+        for (int j = reverse_number.length() - 1; j >=0; --j) {
+            number += reverse_number[j];
         }
+        numbers += number;
+        if (dist*dist == 1) break; // ПРоверка последний ли это символ?
+        numbers += ", "; // Если не последний то добавляем ", "
+        ++count; // Если счетчик равен ограничителю, то добавляем "... "
+        if (limiter != 0 && count == limiter){
+            numbers += "...\n ";
+            count = 0;
+        }
+        (dist > 0) ? ++begin : --begin;
+        dist = end - begin;
     }
     numbers += "]";
     std::cout << numbers << std::endl;
