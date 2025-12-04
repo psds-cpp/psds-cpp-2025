0ed0241a017100c018b12bb249b4151073f14612
GushchinAndrei1 <andreyka.gushchin.03@mail.ru>
2025-12-04 22:49:14 +0500

    реализация func_array

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,28 @@
 #include <stdexcept>
 
 
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(
+    double a,
+    double b,
+    double (*operations[])(double, double),
+    size_t size
+) {
+    if (size == 0) {
+        return 0.0;
+    }
+
+    double sum = 0.0;
+
+    for (size_t i = 0; i < size; ++i) {
+
+        // ---- ключевая защита ----
+        if (operations[i] == nullptr) {
+            continue; // пропускаем пустую функцию
+        }
+
+        double value = operations[i](a, b);
+        sum += value;
+    }
+
+    return sum;
 }
