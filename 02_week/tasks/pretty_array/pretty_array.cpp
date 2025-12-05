5c49bee13efbfb038eca4572573a4a8e91f9c360
Ruslan <ruslan@linux-mint.dlink>
2025-12-05 11:44:27 +0500

    add (solution): add pretty_array task

--- 02_week/tasks/pretty_array/pretty_array.cpp
+++ 02_week/tasks/pretty_array/pretty_array.cpp
@@ -1,6 +1,28 @@
-#include <stdexcept>
+#include <iostream>
 
+void PrintArray(const int* begin, const int* end, size_t limit = 0) {
+    if (!begin || !end) {
+        std::cout << "[]\n";
+        return;
+    }
 
-void PrintArray(/* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+    size_t length = (begin > end) ? (begin - end) : (end - begin);
+
+    std::cout << '[';
+
+    for (size_t i = 0; i < length; ++i) {
+        std::cout << ((begin < end) ? begin[i] : end[length - i]);
+
+        if ((i + 1) == length) {
+            break;
+        }
+
+        std::cout << ", ";
+
+        if ((limit > 0) && ((i + 1) % limit == 0)) {
+            std::cout << "...\n ";
+        }
+    }
+
+    std::cout << "]\n";
 }
