0e3b4bfa0f497c4b1880925b7641472df1e36c3e
Eremin Daniil <danchchik@bk.ru>
2025-12-06 13:01:13 +0500

    add (solution): Решение longest

--- 02_week/tasks/longest/longest.cpp
+++ 02_week/tasks/longest/longest.cpp
@@ -1,6 +1,25 @@
-#include <stdexcept>
+#include <cstddef>
 
 
-/* return_type */ FindLongestSubsequence(/* ptr_type */ begin, /* ptr_type */ end, /* type */ count) {
-    throw std::runtime_error{"Not implemented"};
+char* FindLongestSubsequence(const char* begin, const char* end, size_t& count) {
+    char* res{nullptr};
+    count = 0;
+
+    if(begin == nullptr || end == nullptr) return nullptr;
+
+    size_t curLen{1};
+    char* curBegin{const_cast<char*>(begin)};
+    for(char* curent = const_cast<char*>(begin); curent < end; ++curent){
+        if(curent+1 != end && *curent == *(curent+1)){
+            curBegin = curLen == 1 ? curent : curBegin;
+            ++curLen;
+            continue;
+        }
+
+        res = count < curLen ? curBegin : res;
+        count = count < curLen ? curLen : count;
+        curLen = 1;
+    }
+
+    return res;
 }
