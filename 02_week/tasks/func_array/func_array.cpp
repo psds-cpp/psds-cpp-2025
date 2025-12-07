1ce783f7678d45894527f15fef19ee7dac33bcd6
Polina785643 <polytutynina@gmail.com>
2025-12-07 23:33:07 +0500

    Реализация функции ApplyOperations

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,23 @@
-#include <stdexcept>
+/* Суммирует результаты всех операций из массива, примененных к a и b.*/
+double ApplyOperations(double a, double b, double (**operations)(double, double)
+    , size_t size) {
 
-
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+    double sum = 0.0;
+    
+    // Если массив пустой или size == 0, возвращаем 0.0
+    if (operations == nullptr || size == 0) {
+        return 0.0;
+    }
+    
+    // Проход по всем операциям в массиве
+    for (size_t i = 0; i < size; ++i) {
+        // Проверка того, что операция не является nullptr
+        if (operations[i] != nullptr) {
+            // Выполнение операции и добавление результата к сумме
+            sum += operations[i](a, b);
+        }
+        // Если операция равна nullptr, пропускаем её
+    }
+    
+    return sum;
 }
