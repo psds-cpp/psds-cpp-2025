57da243db07bd3a821c46cd6a79de3b8dfd0b31f
Polina785643 <polytutynina@gmail.com>
2025-12-07 18:40:31 +0500

    Добавить build-* в .gitignore и удалить build-папки из индекса

--- build-asan/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/polina/psds-cpp-2025/build-asan/_deps/googletest-src/googletest
-# Build directory: /home/polina/psds-cpp-2025/build-asan/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build-asan/tasks/print_bits.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstddef>
-#include <stdexcept>
-
-
-void PrintBits(long long value, size_t bytes) {
-    throw std::runtime_error{"Not implemented"};
-}
