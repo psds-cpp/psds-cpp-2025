a43ba2dccd2edbaf44fcbc68e47cdbf36bed06a4
Polina785643 <polytutynina@gmail.com>
2025-12-08 01:59:35 +0500

    Реализация функции FindLongestSubsequence

--- 02_week/tasks/little_big/little_big.cpp
+++ 02_week/tasks/little_big/little_big.cpp
@@ -1,10 +1,43 @@
-#include <stdexcept>
+#include <iostream>
 
+/*Представление числа в памяти*/
 
-void PrintMemory(int /* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+void PrintMemory(int value, bool reverse) {
+    // Оперделение порядока байт
+    const unsigned char* bytes = reinterpret_cast<const unsigned char*>(&value);
+    
+    if (reverse) {
+        // Обратный порядок (little-endian)
+        for (int i = sizeof(value) - 1; i >= 0; --i) {
+            printf("%02X", bytes[i]);
+            if (i > 0) printf(" ");
+        }
+    } else {
+        // Прямой порядок (big-endian)
+        for (size_t i = 0; i < sizeof(value); ++i) {
+            printf("%02X", bytes[i]);
+            if (i < sizeof(value) - 1) printf(" ");
+        }
+    }
+    printf("\n");
 }
 
-void PrintMemory(double /* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+void PrintMemory(double value, bool reverse) {
+    // Оперделение порядока байт
+    const unsigned char* bytes = reinterpret_cast<const unsigned char*>(&value);
+    
+    if (reverse) {
+        // Обратный порядок (little-endian)
+        for (int i = sizeof(value) - 1; i >= 0; --i) {
+            printf("%02X", bytes[i]);
+            if (i > 0) printf(" ");
+        }
+    } else {
+        // Прямой порядок (big-endian)
+        for (size_t i = 0; i < sizeof(value); ++i) {
+            printf("%02X", bytes[i]);
+            if (i < sizeof(value) - 1) printf(" ");
+        }
+    }
+    printf("\n");
 }
