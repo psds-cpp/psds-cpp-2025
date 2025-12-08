982008fc7ca8dc9be0007c902bc65fdb8db271ac
Nikonov Sergey <assassino0@inbox.ru>
2025-12-08 20:10:32 +0500

    add (solution): add func_array task

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,17 @@
 #include <stdexcept>
 
 
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(double a, double b, double (*operations[])(double, double), size_t size ) {
+    double sum = 0.0;
+    
+    if (operations == nullptr || size == 0) {
+        return 0.0;
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+        if (operations[i] != nullptr) {
+            sum += operations[i](a, b);
+        }
+    }
+    return sum;
 }
