412fe66fb04f0c51f2b660dd54e5911b2a820373
Rikitick <rick.tick15@mail.ru>
2025-12-09 01:24:47 +0500

    add (solution): add func_array task

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,11 @@
+#include <cstddef>
 #include <stdexcept>
 
 
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(double a, double b, double (*funcs[])(double a, double b), size_t size) {
+    double res = 0.0;
+    for (size_t i = 0; i < size; ++i)
+        if (funcs[i])
+            res += funcs[i](a, b);
+    return res;
 }
