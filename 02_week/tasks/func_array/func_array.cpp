afdacf722efd2f04bd57fcd29d87c8b06719d146
Rychkov Grigoriy <skeletron932fc@gmail.com>
2025-12-09 21:17:45 +0500

    Func_array attempt 1

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,15 @@
 #include <stdexcept>
 
-
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(double a, double b, double (**operations)(double, double), size_t n) {
+    double total = 0.0;
+    
+    if (operations == nullptr || n == 0) {
+        return total;}
+    
+    for (size_t i = 0; i < n; ++i) {
+        if (operations[i] != nullptr) {
+            total += operations[i](a, b);}
+    }
+    
+    return total;
 }
