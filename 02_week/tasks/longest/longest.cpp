cb169138f3266cf6e4a0b2a2f2ca1f8d32c45703
KaigorodovMM <kai-tram-pam-pam@mail.ru>
2025-12-09 23:10:34 +0500

    add solution: add longest task

--- 02_week/tasks/longest/longest.cpp
+++ 02_week/tasks/longest/longest.cpp
@@ -1,6 +1,30 @@
 #include <stdexcept>
 
 
-/* return_type */ FindLongestSubsequence(/* ptr_type */ begin, /* ptr_type */ end, /* type */ count) {
-    throw std::runtime_error{"Not implemented"};
+char* FindLongestSubsequence(const char* begin, const char* end, size_t& count) {
+    count = 0;
+    if ((begin >= end) or (begin == nullptr) or (end == nullptr)){
+        return nullptr;
+    }
+    char* long_begin = const_cast<char*>(begin);
+    //char long_char = *begin;
+    size_t len = 0;
+    char* char0 = const_cast<char*>(begin);
+    while (begin <= end){
+        if(*char0 == *begin){
+            len++;
+        }
+        else{
+            if (len > count){
+                count = len;
+                //long_char = char0;
+                long_begin = const_cast<char*>(char0);
+            }
+            char0 = const_cast<char*>(begin);
+            len = 1;
+        }
+
+        begin++;
+    }
+    return long_begin;
 }
