a7eefbec3842f84d57c02ac9218a8dc2447fd837
Rikitick <rick.tick15@mail.ru>
2025-12-09 22:09:35 +0500

    add (solution): add pretty_array task

--- 02_week/tasks/pretty_array/pretty_array.cpp
+++ 02_week/tasks/pretty_array/pretty_array.cpp
@@ -1,6 +1,35 @@
 #include <stdexcept>
+#include <iostream>
 
+void PrintArray(const int* begin, const int* end, size_t limit = 0) {
+    const bool reverse = (begin > end);
+    const size_t len = std::abs(end - begin);
 
-void PrintArray(/* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+    if (len == 0) {
+        std::cout << "[]\n";
+        return;
+    }
+
+    const int* cur = begin;
+    size_t cnt = 0;
+    
+    std::cout << "[";
+
+    for (size_t i = 0; i < len; ++i) {
+        std::cout << *cur; 
+        if (i != len - 1) {
+            if ((limit > 0) && (cnt == limit - 1)) {
+                std::cout << ", ...\n ";
+                cnt = 0;
+            } 
+            else {
+                std::cout << ", ";
+                ++cnt;
+            }
+        }
+        
+        if (i != len - 1)
+            cur = reverse ? cur - 1 : cur + 1;
+    }
+    std::cout << "]\n";
 }
