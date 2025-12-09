bd129ad659e2df2bdf6e4d7abf090df44801d7d7
karskanovas <yuktyui47@gmail.com>
2025-12-09 21:16:46 +0500

    func_array

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,12 @@
-#include <stdexcept>
+#include <cstddef> 
 
-
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(double a, double b, double (*operations[])(double, double), size_t size) {
+    if (size == 0 || operations == nullptr) {
+        return 0.0;}
+    double sum = 0.0;
+    for (size_t i = 0; i < size; ++i) {
+        if (operations[i] != nullptr) {  
+            sum += operations[i](a, b);
+        } }
+    return sum;
 }
