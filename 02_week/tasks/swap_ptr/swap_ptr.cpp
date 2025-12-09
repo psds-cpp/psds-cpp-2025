c0bd5f402b152f1ac8a7c9b67c3cbea7d7683a00
KaigorodovMM <kai-tram-pam-pam@mail.ru>
2025-12-09 21:58:59 +0500

    add solution: add swap_ptr task

--- 02_week/tasks/swap_ptr/swap_ptr.cpp
+++ 02_week/tasks/swap_ptr/swap_ptr.cpp
@@ -1,6 +1,19 @@
 #include <stdexcept>
 
 
-void SwapPtr(/* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+void SwapPtr(int*& ptr1, int*& ptr2) {
+    int* ptr0 = ptr1;
+    ptr1 = ptr2;
+    ptr2 = ptr0;
 }
+void SwapPtr(const int*& ptr1, const int*& ptr2) {
+    const int* ptr0 = ptr1;
+    ptr1 = ptr2;
+    ptr2 = ptr0;
+}
+void SwapPtr(int**& ptr1, int**& ptr2) {
+    int** ptr0 = ptr1;
+    ptr1 = ptr2;
+    ptr2 = ptr0;
+}
+
