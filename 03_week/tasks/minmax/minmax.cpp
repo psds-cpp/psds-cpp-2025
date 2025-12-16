495147e0b6de8fc55f57ad8fd23ac458832f564d
Lenar Gatin <lgatin1711@gmail.com>
2025-12-16 20:13:57 +0500

    add (solution): add minmax task

--- 03_week/tasks/minmax/minmax.cpp
+++ 03_week/tasks/minmax/minmax.cpp
@@ -1,6 +1,28 @@
-#include <stdexcept>
+#include <vector>
+#include <stddef.h>
 
 
-/* return_type */ MinMax(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+struct MinMaxResult {
+    std::vector<int>::const_iterator min_it{};
+    std::vector<int>::const_iterator max_it{};
+};
+
+
+MinMaxResult MinMax(const std::vector<int>& arr) {
+    MinMaxResult result{arr.end(), arr.end()};
+
+    if (arr.empty()) return result;
+
+    result = {arr.begin(), arr.begin()};
+
+    for (size_t i = 1; i < arr.size(); ++i) {
+        if (arr[i] >= *result.max_it) {
+            result.max_it = arr.begin() + i;
+        }
+        else if (arr[i] < *result.min_it) {
+            result.min_it = arr.begin() + i;
+        }
+    }
+
+    return result;
 }
