b5b6bc3ad1682d651d447c359d9a0a1157c4eab6
Dsv9toy <danilka6932@mail.ru>
2025-12-17 18:24:49 +0500

    Добавлен filter

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,29 @@
 #include <stdexcept>
+#include <vector>
 
+/*
+Функция принимает std::vector<int>
+и указатель на функцию предикат, и оставляет в контейнере элементы, 
+удовлетворяющие предикату, в исходном порядке. 
+Нельзя алгоритм
+Алгоритм долэен проходить за O(n)
+*/
 
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+using PredicatePtr = bool(*)(int);
+void Filter(std::vector<int>& array, PredicatePtr predicate) {
+    if (!predicate) return;
+
+    
+    size_t writeIndex = 0;
+    
+    for (size_t readIndex = 0; readIndex < array.size(); ++readIndex) {
+        if (predicate(array[readIndex])) {
+            if (writeIndex != readIndex) {
+                array[writeIndex] = std::move(array[readIndex]);
+            }
+            ++writeIndex;
+        }
+    }
+    
+    array.resize(writeIndex);
 }
