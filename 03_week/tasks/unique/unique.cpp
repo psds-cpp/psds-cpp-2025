91f363ce632e8aaf7dec444b79f5079036788b09
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:09:19 +0500

    add (solution): add unique task

--- 03_week/tasks/unique/unique.cpp
+++ 03_week/tasks/unique/unique.cpp
@@ -1,6 +1,30 @@
-#include <stdexcept>
 #include <vector>
 
-/* return_type */ Unique(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+auto Unique(const std::vector<int>& vec) {
+    std::vector<int> res;
+
+    if (vec.empty()) {
+        return res;
+    }
+
+    size_t count = 1;
+
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        
+        if (vec[i] != vec[i + 1]) {
+            ++count;
+        }
+    }
+
+    res.reserve(count);
+    res.push_back(vec[0]);
+
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        
+        if (vec[i] != vec[i + 1]) {
+            res.push_back(vec[i + 1]);
+        }
+    }
+
+    return res;
 }
