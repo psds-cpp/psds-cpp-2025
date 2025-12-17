483d3ffe6a85cfd46ea11c4f44cd28d28cde4a65
Di0nisP <ya.amico@yandex.ru>
2025-12-17 21:32:05 +0500

    feat (03_week): add working implementation

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,16 @@
-#include <stdexcept>
+#include <vector>
 
 
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+void Filter(std::vector<int>& vec, bool (*predicate)(int)) {
+    if (vec.empty() || predicate == nullptr) {
+        return;
+    }
+    
+    size_t write {}; // куда писать "валидные" элементы
+    for (std::size_t read = 0; read < vec.size(); ++read) {
+        if (predicate(vec[read])) {
+            vec[write++] = vec[read];
+        }
+    }
+    vec.resize(write); // обрезаем хвост
 }
