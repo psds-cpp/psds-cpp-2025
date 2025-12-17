a91e20f960f0cf6e5933edcc610149563e66ed4c
Nicolay Petrov <lilpip2002@mail.ru>
2025-12-17 22:18:14 +0500

    add (solution): add easy_compare task

--- 03_week/tasks/easy_compare/easy_compare.cpp
+++ 03_week/tasks/easy_compare/easy_compare.cpp
@@ -2,9 +2,9 @@
 
 
 struct Date {
-    unsigned year;
-    unsigned month;
-    unsigned day;
+    unsigned year = 0;
+    unsigned month = 0;
+    unsigned day = 0;
 };
 
 struct StudentInfo {
@@ -14,3 +14,60 @@ struct StudentInfo {
     unsigned course;
     Date birth_date;
 };
+//
+bool operator < (const Date& lhs, const Date& rhs){
+   return std::tie(lhs.year, lhs.month, lhs.day) < std::tie(rhs.year, rhs.month, rhs.day);
+}
+bool operator == (const Date& lhs, const Date& rhs){
+   return std::tie(lhs.year, lhs.month, lhs.day) == std::tie(rhs.year, rhs.month, rhs.day);
+}
+bool operator!=(const Date& lhs, const Date& rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator<=(const Date& lhs, const Date& rhs) {
+    return (lhs < rhs) || (lhs == rhs);
+}
+
+bool operator>(const Date& lhs, const Date& rhs) {
+    return !(lhs <= rhs);  
+}
+
+bool operator>=(const Date& lhs, const Date& rhs) {
+    return !(lhs < rhs);  
+}
+//
+bool operator == (const StudentInfo& lhs, const StudentInfo& rhs){
+    return std::tie(lhs.mark, lhs.score) == std::tie(rhs.mark, rhs.score);
+}
+
+bool operator<(const StudentInfo& lhs, const StudentInfo& rhs) {
+    if (lhs.mark != rhs.mark) {
+        return rhs.mark < lhs.mark;  
+    }
+    
+    if (lhs.score != rhs.score) {
+        return rhs.score > lhs.score; 
+    }
+    
+    if (lhs.course != rhs.course) {
+        return lhs.course > rhs.course;
+    }
+    
+    return lhs.birth_date < rhs.birth_date;
+}
+bool operator!=(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator<=(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return (lhs < rhs) || (lhs == rhs);
+}
+
+bool operator>(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return !(lhs <= rhs);
+}
+
+bool operator>=(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return !(lhs < rhs);
+}
