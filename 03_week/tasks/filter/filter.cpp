668c74f92218805b84a016f9c244108069171cb3
leff <lev.king.2035@gmail.com>
2025-12-17 20:59:29 +0500

    add (solution): add filter task

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,23 @@
 #include <stdexcept>
+#include <vector>
 
-
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+void Filter(std::vector<int> &data, bool (*func)(int))
+{
+    if (!data.empty() && func != nullptr)
+    {
+        size_t counter = 0;
+       
+        for (size_t i = 0; i < data.size(); ++i)
+        {
+            if (func(data[i]))
+            {
+                if (i!= counter){
+                    data[counter] = data[i];
+                }
+                ++counter;
+            }
+        }   
+        data.resize(counter);
+    }
+    return;
 }
