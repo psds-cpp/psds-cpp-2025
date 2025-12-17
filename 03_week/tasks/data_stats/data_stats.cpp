6693bc7ec0b457a1539059e83060ef4d93740954
Nicolay Petrov <lilpip2002@mail.ru>
2025-12-17 18:28:12 +0500

    add (solution): add data_stats task

--- 03_week/tasks/data_stats/data_stats.cpp
+++ 03_week/tasks/data_stats/data_stats.cpp
@@ -1,4 +1,6 @@
 #include <stdexcept>
+#include <vector>
+#include <cmath>
 
 
 struct DataStats {
@@ -6,6 +8,29 @@ struct DataStats {
     double sd = 0.0;
 };
 
-double CalculateDataStats(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+DataStats CalculateDataStats(const std::vector<int>& vec) {
+    if (vec.empty()) {
+        return DataStats{0.0, 0.0};
+    }
+
+    long double sum = 0.0;
+    long double sum_sq = 0.0;
+    
+    for (int x : vec) {
+        long double ld_x = static_cast<long double>(x);
+        sum += ld_x;
+        sum_sq += ld_x * ld_x;
+    }
+    
+    long double avg = sum / vec.size();
+    long double variance = (sum_sq / vec.size()) - (avg * avg);
+    
+    // Защита от ошибок округления
+    if (variance < 0.0) {
+        variance = 0.0;
+    }
+    
+    double sd = static_cast<double>(std::sqrt(variance));
+    
+    return DataStats{static_cast<double>(avg), sd};
 }
