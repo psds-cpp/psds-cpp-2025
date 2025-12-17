145ca6cd52c7882bf91d1a95d74aa139d32cbf10
Nikonov Sergey <assassino0@inbox.ru>
2025-12-17 20:32:31 +0500

    add (solution): add little_big task

--- 02_week/tasks/little_big/little_big.cpp
+++ 02_week/tasks/little_big/little_big.cpp
@@ -9,11 +9,9 @@ void PrintMemory(int value, bool reverse = false) {
     std::memcpy(&bytes, &value, sizeof(value));
     
     if (reverse) {
-        // Оригинальный порядок байт (big-endian представление)
         std::cout << "0x" << std::uppercase << std::hex << std::setfill('0') << std::setw(8) 
                   << bytes << "\n";
     } else {
-        // Little-endian представление (байты в обратном порядке)
         uint32_t reversed = 0;
         for (size_t i = 0; i < sizeof(value); ++i) {
             reversed = (reversed << 8) | ((bytes >> (i * 8)) & 0xFF);
@@ -28,11 +26,9 @@ void PrintMemory(double value, bool reverse = false) {
     std::memcpy(&bytes, &value, sizeof(value));
     
     if (reverse) {
-        // Оригинальный порядок байт (big-endian представление)
         std::cout << "0x" << std::uppercase << std::hex << std::setfill('0') << std::setw(16) 
                   << bytes << "\n";
     } else {
-        // Little-endian представление (байты в обратном порядке)
         uint64_t reversed = 0;
         for (size_t i = 0; i < sizeof(value); ++i) {
             reversed = (reversed << 8) | ((bytes >> (i * 8)) & 0xFF);
