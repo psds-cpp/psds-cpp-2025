1b1360ba5eac1996d3cf0c481d48175051193c03
Rychkov Grigoriy <skeletron932fc@gmail.com>
2025-12-19 21:52:44 +0500

    unique attempt 1

--- 03_week/tasks/unique/unique.cpp
+++ 03_week/tasks/unique/unique.cpp
@@ -1,6 +1,25 @@
 #include <stdexcept>
 #include <vector>
 
-/* return_type */ Unique(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::vector<int> Unique(const std::vector<int>& vec) {
+    std::vector<int> result;
+    
+    if (vec.empty()) {
+        return result;  
+    }
+    
+    result.reserve(vec.size());
+    
+    
+    result.push_back(vec[0]);
+    
+    for (size_t i = 1; i < vec.size(); ++i) {
+        if (vec[i] != result.back()) {
+            result.push_back(vec[i]);
+        }
+    }
+    
+    result.shrink_to_fit();
+    
+    return result;
 }
