aa36ec472b4134540ea247210b8f2bda15880ac5
Rychkov Grigoriy <skeletron932fc@gmail.com>
2025-12-19 21:51:26 +0500

    minmax attempt 1

--- 03_week/tasks/minmax/minmax.cpp
+++ 03_week/tasks/minmax/minmax.cpp
@@ -1,6 +1,50 @@
 #include <stdexcept>
+#include <vector>
+#include <utility>
 
+std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> 
+MinMax(const std::vector<int>& vec) {
+    if (vec.empty()) {
+        return {vec.end(), vec.end()};
+    }
+    
+    auto min_it = vec.begin();
+    auto max_it = vec.begin();
+    
+    for (auto it = vec.begin() + 1; it != vec.end(); ++it) {
+        if (*it < *min_it) {
+            
+            min_it = it;
+        } else if (*it > *max_it) {
+            
+            max_it = it;
+        } else if (*it == *max_it) {
+            
+            max_it = it;
+        }
+    }
+    
+    return {min_it, max_it};
+}
 
-/* return_type */ MinMax(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::pair<std::vector<int>::iterator, std::vector<int>::iterator> 
+MinMax(std::vector<int>& vec) {
+    if (vec.empty()) {
+        return {vec.end(), vec.end()};
+    }
+    
+    auto min_it = vec.begin();
+    auto max_it = vec.begin();
+    
+    for (auto it = vec.begin() + 1; it != vec.end(); ++it) {
+        if (*it < *min_it) {
+            min_it = it;
+        } else if (*it > *max_it) {
+            max_it = it;
+        } else if (*it == *max_it) {
+            max_it = it;
+        }
+    }
+    
+    return {min_it, max_it};
 }
