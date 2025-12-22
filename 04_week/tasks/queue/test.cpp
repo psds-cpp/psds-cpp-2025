03688a015aa2d1fa274f372fdc3dea0a3b1e06fa
Sergei Dekhtiar <102069117+18thday@users.noreply.github.com>
2025-12-22 20:24:55 +0500

    Update test.cpp

--- 04_week/tasks/queue/test.cpp
+++ 04_week/tasks/queue/test.cpp
@@ -374,7 +374,7 @@ protected:
 };
 
 TEST_F(QueuePerformanceTest, CapacityConstructor) {
-    constexpr int LARGE_SIZE = 50'000'000;
+    constexpr int LARGE_SIZE = 10'000'000;
     Queue q_with_reserve(LARGE_SIZE);
 
     long long time_with_reserve = MeasureTime([&q_with_reserve]() {
@@ -415,7 +415,7 @@ TEST_F(QueuePerformanceTest, CapacityConstructor) {
               << (100.0 * (time_without_reserve - time_with_reserve) / time_without_reserve)
               << "%)" << std::endl;
 
-    EXPECT_LT(time_with_reserve * 1.0, time_without_reserve * 0.9);
+    EXPECT_LT(time_with_reserve * 1.0, time_without_reserve * 0.94);
 }
 
 TEST_F(QueuePerformanceTest, CapacityCtorCompareWithVector) {
