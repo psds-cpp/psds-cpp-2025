6f857f6805a883b05c68704b545d0f88873b47b7
Lenar Gatin <lgatin1711@gmail.com>
2025-12-29 15:16:08 +0500

    ref (solution): remove dynamic array buffer and output directly

--- 01_week/tasks/print_bits/print_bits.cpp
+++ 01_week/tasks/print_bits/print_bits.cpp
@@ -3,27 +3,20 @@
 
 
 void PrintBits(long long value, size_t bytes) {
-    long long bit_index = 1ll;  // 0b0000'...'000
-
     size_t bits = bytes * 8;
+    unsigned long long bit_index = 1ll << (bits - 1);  // 0b1000'...'0000
     size_t delims = 1 + 2 * (bytes - 1);
-
     // Выделяем буфер под биты, апострофы и '\0' в конце
     size_t buffer_size = bits + delims + 1;
 
-    char* buffer = new char[buffer_size] {'\0'};
-
-    for (int i = buffer_size - 2; i >= 0; --i) {
+    std::cout << "0b";
+    for (size_t i = 0; i < buffer_size - 1; ++i) {
         if (i % 5 == 4) {
-            buffer[i] = '\'';
+            std::cout << '\'';
         } else {
-            buffer[i] = static_cast<bool>(value & bit_index) + '0';
-            bit_index = bit_index << 1;
+            std::cout << static_cast<char>(static_cast<bool>(value & bit_index) + '0');
+            bit_index = bit_index >> 1;
         }
     }
-    
-    std::cout << "0b" << buffer << std::endl;
-
-    delete[] buffer;
-    buffer = nullptr;
+    std::cout << std::endl;
 }
