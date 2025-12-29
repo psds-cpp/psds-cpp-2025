e586b2014de3cf2b822d0337fa5679dd8787bbe2
Lenar Gatin <lgatin1711@gmail.com>
2025-12-29 16:11:27 +0500

    ref (solution): resolve undefined behaviour with const_cast

--- 02_week/tasks/last_of_us/last_of_us.cpp
+++ 02_week/tasks/last_of_us/last_of_us.cpp
@@ -1,14 +1,30 @@
-int* FindLastElement(const int* begin, const int* end, bool (*predicate)(int)) {
-    if (!begin || !end || !predicate || begin > end)
-        return const_cast<int*>(end);
+#include <cstddef>
 
-    int* last = const_cast<int*>(end);
+const int* FindLastElement(const int* begin, const int* end, bool (*predicate)(int)) {
+    if (!begin || !end || !predicate || begin > end)
+        return end;
+
+    const int* last = end;
 
     for (; begin < end; ++begin) {
         if (predicate(*begin)) {
-            last = const_cast<int*>(begin);
+            last = begin;
         }
     }
 
     return last;
+}
+
+int* FindLastElement(int* begin, int* end, bool (*predicate)(int)) {
+    return const_cast<int*>(
+        FindLastElement(
+            const_cast<const int*>(begin), 
+            const_cast<const int*>(end), 
+            predicate
+        )
+    );
+}
+
+inline int* FindLastElement(std::nullptr_t, std::nullptr_t, bool (*)(int)) {
+    return nullptr;
 }
