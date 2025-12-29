508d5a76f599ef1e5257eaf93f9cbde213d74c6c
Lenar Gatin <lgatin1711@gmail.com>
2025-12-29 17:35:51 +0500

    ref (solution): unify style of fields initialization and join separate output of symbols

--- 03_week/tasks/os_overload/os_overload.cpp
+++ 03_week/tasks/os_overload/os_overload.cpp
@@ -9,7 +9,7 @@ struct Coord2D {
 };
 
 struct Circle {
-    Coord2D coord{};
+    Coord2D coord = {};
     unsigned radius = 1;
 };
 
@@ -36,8 +36,7 @@ std::ostream& operator<<(std::ostream& lhs, const CircleRegionList& rhs) {
     lhs << "{";
 
     if (!rhs.empty()) {
-        lhs << "\n";
-        lhs << '\t' << rhs[0];
+        lhs << "\n\t" << rhs[0];
     }
 
     for (size_t i = 1; i < rhs.size(); ++i) {
