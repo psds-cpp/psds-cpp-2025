3ff40839bb3ecf288aac1c2127ea2e3988883515
Lenar Gatin <lgatin1711@gmail.com>
2025-12-29 14:48:40 +0500

    ref (solution): rewrite ineffective solution

--- 01_week/tasks/check_flags/check_flags.cpp
+++ 01_week/tasks/check_flags/check_flags.cpp
@@ -19,53 +19,36 @@ CheckFlags operator&(CheckFlags lhs, CheckFlags rhs) {
     return static_cast<CheckFlags>(static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs));
 }
 
-std::string GetFlagName(CheckFlags flags) {
-    switch (flags) {
-        case CheckFlags::TIME:
-            return "TIME";
-        case CheckFlags::DATE:
-            return "DATE";
-        case CheckFlags::USER:
-            return "USER";
-        case CheckFlags::CERT:
-            return "CERT";
-        case CheckFlags::KEYS:
-            return "KEYS";
-        case CheckFlags::DEST:
-            return "DEST";
-        default:
-            return "";
-    }
-}
-
 void PrintCheckFlags(CheckFlags flags) {
     // Проверка на выход за диапозон значений CheckFlags
-    if (static_cast<int>(CheckFlags::ALL) < static_cast<int>(flags)) {
+    if ((~static_cast<uint8_t>(CheckFlags::ALL) & static_cast<uint8_t>(flags)) != 0) {
         return;
     }
 
-    std::vector<CheckFlags> checks{
-        CheckFlags::TIME, 
-        CheckFlags::DATE,
-        CheckFlags::USER,
-        CheckFlags::CERT,
-        CheckFlags::KEYS,
-        CheckFlags::DEST,
+    const static std::pair<CheckFlags, std::string> checks[] {
+        {CheckFlags::TIME, "TIME"},
+        {CheckFlags::DATE, "DATE"},
+        {CheckFlags::USER, "USER"},
+        {CheckFlags::CERT, "CERT"},
+        {CheckFlags::KEYS, "KEYS"},
+        {CheckFlags::DEST, "DEST"}
     };
 
-    std::vector<std::string> flags_names{};
-    for (auto &check : checks) {
-        if (std::string flag_name = GetFlagName(flags & check); flag_name != "") {
-            flags_names.push_back(flag_name);
+    bool first = true;
+
+    std::cout << "[";
+
+    for (const auto& [flag, name] : checks) {
+        if ((flags & flag) != CheckFlags::NONE) {
+            if (first) {
+                first = false;
+            }
+            else {
+                std::cout << ",";
+            }
+            std::cout << name;
         }
     }
 
-    std::string result = "[";
-    for (size_t i = 0, flags_size = flags_names.size(); i < flags_size; ++i) {
-        result += flags_names[i];
-        if (i != flags_size - 1) {
-            result += ',';
-        }
-    }
-    std::cout << result << ']';
+    std::cout << ']';
 }
