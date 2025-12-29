35ad4a702fd62ca5a2f2ccfff8eb10dc28006313
Lenar Gatin <lgatin1711@gmail.com>
2025-12-29 16:59:35 +0500

    ref (solution): pass struct by reference and replace if-branching with std::tie

--- 03_week/tasks/easy_compare/easy_compare.cpp
+++ 03_week/tasks/easy_compare/easy_compare.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <stddef.h>
 
-
 struct Date {
     unsigned year = 0u;
     unsigned month = 0u;
@@ -17,61 +16,53 @@ struct StudentInfo {
     Date birth_date;
 };
 
-bool operator<(Date lhs, Date rhs) {
+bool operator<(const Date& lhs, const Date& rhs) {
     return std::tie(lhs.year, lhs.month, lhs.day) < std::tie(rhs.year, rhs.month, rhs.day);
 }
 
-bool operator==(Date lhs, Date rhs) {
+bool operator==(const Date& lhs, const Date& rhs) {
     return std::tie(lhs.year, lhs.month, lhs.day) == std::tie(rhs.year, rhs.month, rhs.day);
 }
 
-bool operator!=(Date lhs, Date rhs) {
+bool operator!=(const Date& lhs, const Date& rhs) {
     return !(lhs == rhs);
 }
 
-bool operator>(Date lhs, Date rhs) {
+bool operator>(const Date& lhs, const Date& rhs) {
     return !(lhs < rhs || lhs == rhs);
 }
 
-bool operator>=(Date lhs, Date rhs) {
+bool operator>=(const Date& lhs, const Date& rhs) {
     return !(lhs < rhs);
 }
 
-bool operator<=(Date lhs, Date rhs) {
+bool operator<=(const Date& lhs, const Date& rhs) {
     return lhs < rhs || lhs == rhs;
 }
 
-bool operator<(StudentInfo& lhs, StudentInfo& rhs) {
-    if (lhs.mark != rhs.mark) {
-        return lhs.mark > rhs.mark;
-    }
-    else if (lhs.score != rhs.score) {
-        return lhs.score < rhs.score;
-    }
-    else if (lhs.course != rhs.course) {
-        return lhs.course > rhs.course;
-    }
-    else {
-        return lhs.birth_date < rhs.birth_date;
-    }
+bool operator<(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return (
+        std::tie(rhs.mark, lhs.score, rhs.course, lhs.birth_date)
+        < std::tie(lhs.mark, rhs.score, lhs.course, rhs.birth_date)
+    );
 }
 
-bool operator==(StudentInfo& lhs, StudentInfo& rhs) {
+bool operator==(const StudentInfo& lhs, const StudentInfo& rhs) {
     return lhs.mark == rhs.mark && lhs.score == rhs.score;
 }
 
-bool operator!=(StudentInfo& lhs, StudentInfo& rhs) {
+bool operator!=(const StudentInfo& lhs, const StudentInfo& rhs) {
     return !(lhs == rhs);
 }
 
-bool operator>(StudentInfo& lhs, StudentInfo& rhs) {
+bool operator>(const StudentInfo& lhs, const StudentInfo& rhs) {
     return !(lhs < rhs || lhs == rhs);
 }
 
-bool operator>=(StudentInfo& lhs, StudentInfo& rhs) {
+bool operator>=(const StudentInfo& lhs, const StudentInfo& rhs) {
     return !(lhs < rhs);
 }
 
-bool operator<=(StudentInfo& lhs, StudentInfo& rhs) {
+bool operator<=(const StudentInfo& lhs, const StudentInfo& rhs) {
     return lhs < rhs || lhs == rhs;
 }
