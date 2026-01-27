83f113dce0e2be9a57ba4309ebe8ab33bccab859
leff <lev.king.2035@gmail.com>
2026-01-27 17:22:52 +0500

    add minmax task

--- 03_week/tasks/minmax/minmax.cpp
+++ 03_week/tasks/minmax/minmax.cpp
@@ -1,6 +1,36 @@
 #include <stdexcept>
+#include <vector>
+#include <tuple>
 
+std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> MinMax(const std::vector<int> &data)
+{
 
-/* return_type */ MinMax(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+    if (!data.empty())
+    {
+        std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> counters = {data.cbegin(), data.cbegin()};
+        size_t size = data.size();
+
+        for (size_t i = 0; i < size; ++i)
+        {
+            std::vector<int>::const_iterator it = data.cbegin() + i;
+            if (*it < *counters.first)
+            {
+                counters.first = it;
+            }
+            else
+            {
+                if (*it >= *counters.second)
+                {
+                    counters.second = it;
+                }
+            }
+        }
+
+        return counters;
+    }
+    else
+    {
+        std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> counters;
+        return counters;
+    }
 }
