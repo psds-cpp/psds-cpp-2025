50876c11450563ad9a29531e84c852420df8ec7d
leff <lev.king.2035@gmail.com>
2026-01-27 17:19:07 +0500

    add range task

--- 03_week/tasks/range/range.cpp
+++ 03_week/tasks/range/range.cpp
@@ -1,7 +1,21 @@
 #include <stdexcept>
 #include <vector>
+#include <cmath>
 
 
-std::vector<int> Range(int from, int to, int step) {
-    throw std::runtime_error{"Not implemented"};
+std::vector<int> Range(int from, int to, int step = 1) {
+    long long it = static_cast<long long>(std::ceil((to - from)/static_cast<double>(step)));
+    if (step != 0 && to != from && it > 0){
+        std::vector<int> result;
+        result.reserve(it);
+        for (size_t i = 0; i < static_cast<size_t>(it); ++i){
+            result.push_back(from + step*i);
+        }
+        result.shrink_to_fit();
+        return result;
+    }
+    else{
+        std::vector<int> null;
+        return null;
+    }
 }
