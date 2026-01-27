2924be5bc0b5ffe980cad1dc8c5698b65c285ef1
leff <lev.king.2035@gmail.com>
2026-01-27 17:21:23 +0500

    add unique task

--- 03_week/tasks/unique/unique.cpp
+++ 03_week/tasks/unique/unique.cpp
@@ -1,6 +1,21 @@
 #include <stdexcept>
 #include <vector>
 
-/* return_type */ Unique(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::vector<int> Unique(const std::vector<int>& v) {
+    std::vector<int> result;
+    result.reserve(v.size());
+    for (size_t i = 0; i < v.size(); ++i){
+        bool is_in_result = false;
+        for (size_t k = 0; k < result.size(); ++k){
+            if (result[k] == v[i]){
+                is_in_result = true;
+            }
+        }
+        if (!is_in_result){
+            result.push_back(v[i]);
+        }
+
+    }
+    result.shrink_to_fit();
+    return result;
 }
