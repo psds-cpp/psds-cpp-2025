876338ac231678fa38336738c3e0531a76663f27
Ilya Mikhailov <ilyamikhailov16@yandex.ru>
2026-02-08 16:36:33 +0500

    Update char_changer.cpp

--- 01_week/tasks/char_changer/char_changer.cpp
+++ 01_week/tasks/char_changer/char_changer.cpp
@@ -6,23 +6,23 @@ size_t CharChanger(char array[], size_t, char delimiter = ' ') {
     int counter = 0; // Счётчик повторяющихся символов
     int pos_write = 0;   // Индекс для записи обработанного символа
     int pos_read = 0;    // Индекс для чтения следующего элемента из массива
-    char repeating_symbol = array[read]; // В процессе выполнения - редыдущий символ после read(отслеживаем повторения)
+    char repeating_symbol = array[pos_read]; // В процессе выполнения - предыдущий символ после pos_read(отслеживаем повторения)
 
     while (repeating_symbol != '\0'){
-        if (repeating_symbol == array[read]){
+        if (repeating_symbol == array[pos_read]){
             ++counter;
         } else {
             if (isalpha(repeating_symbol)){
-                array[write] = toupper(repeating_symbol);
+                array[pos_write] = toupper(repeating_symbol);
             } else if (isdigit(repeating_symbol)){
-                array[write] = '*';
+                array[pos_write] = '*';
             } else if (repeating_symbol == ' '){
-                array[write] = delimiter;
+                array[pos_write] = delimiter;
             } else {
-                array[write] = '_';
+                array[pos_write] = '_';
             }
 
-            ++write;
+            ++pos_write;
 
             if (repeating_symbol == ' ') {
                 counter = 1;
@@ -33,15 +33,15 @@ size_t CharChanger(char array[], size_t, char delimiter = ' ') {
             }
 
             if (counter != 1){
-                array[write] = static_cast<char>(counter + '0'); // Преобразуем число в символ
+                array[pos_write] = static_cast<char>(counter + '0'); // Преобразуем число в символ
                 counter = 1;
-                ++write;
+                ++pos_write;
             }
 
-            repeating_symbol = array[read];
+            repeating_symbol = array[pos_read];
         }
 
-        ++read;
+        ++pos_read;
     }
 
     array[write] = '\0';
