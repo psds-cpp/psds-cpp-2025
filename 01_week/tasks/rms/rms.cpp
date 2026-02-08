eea9f87613e9dfdd2eb3d68130b59a6c0e1ebde0
Ilya Mikhailov <ilyamikhailov16@yandex.ru>
2026-02-08 14:42:28 +0500

    Update rms.cpp

--- 01_week/tasks/rms/rms.cpp
+++ 01_week/tasks/rms/rms.cpp
@@ -6,7 +6,7 @@ double CalculateRMS(double values[], size_t size) {
     if (size <= 0 || values == nullptr){ return 0.0; }
 
     double sum = 0;
-    for (size_t i=0; i < size; i++){
+    for (size_t i=0; i < size; ++i){
         sum+=std::pow(values[i], 2);
     }
 
