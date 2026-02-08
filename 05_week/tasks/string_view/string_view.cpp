2bf0972d87a7fde1f3de64eb0ca6234bb7ac4baa
Radiy <notron12460@gmail.com>
2026-02-08 21:33:31 +0500

    add (solution): add string_view task

--- 05_week/tasks/string_view/string_view.cpp
+++ 05_week/tasks/string_view/string_view.cpp
@@ -1,7 +1,180 @@
 #include <cstring>
+#include <limits>
+#include <optional>
 #include <string>
+#include <utility>
 
 
 class StringView {
+public:
+    inline static size_t npos = std::numeric_limits<size_t>::max();
+    
+    StringView() = default;
+    
+    StringView(const std::string& str, size_t start = 0, size_t len = npos) {
+        if (start > str.size() || str.empty()) {
+            return;
+        }
 
+        if (len == npos || start + len > str.size()) {
+            len = str.size() - start;
+        } 
+
+        begin_ = str.data() + start;
+        end_ = begin_ + len;
+    };
+
+    StringView(const std::string&& str, size_t start = 0, size_t len = npos) {
+        if (start > str.size() || str.empty()) {
+            return;
+        }
+        
+        tempStr_ = std::move(str);
+
+
+        if (len == npos || start + len > tempStr_->size()) {
+            len = tempStr_->size() - start;
+        } 
+
+        begin_ = tempStr_->data() + start;
+        end_ = begin_ + len;
+    };
+
+    StringView(const char* str) : begin_(str ? str : nullptr), end_(str ? str + std::strlen(str) : nullptr) {}
+
+    StringView(const char* str, size_t len) {
+        if (!str) {
+            return;
+        }
+
+        size_t strLen = std::strlen(str);
+        if (len > strLen) {
+            len = strLen;
+        }
+
+        begin_ = str;
+        end_ = str + len;
+    }
+
+    const char& operator[](size_t idx) const {
+        return begin_[idx];
+    }
+
+    const char* Data() const;
+    const char& Front() const;
+    const char& Back() const;
+    size_t Size() const;
+    size_t Length() const;
+    bool Empty() const;
+    int Compare(StringView& str) const;
+    void RemovePrefix(size_t len);
+    void RemoveSuffix(size_t len);
+    StringView Substr(size_t start, size_t len) const;
+    size_t Find(char symbol, size_t start) const;
+    size_t Find(StringView str, size_t start) const;
+    std::string ToString() const;
+    
+private:
+    std::optional<std::string> tempStr_;
+    const char* begin_ = nullptr;
+    const char* end_ = nullptr;
 };
+
+
+bool operator==(StringView& lhs, StringView& rhs) {
+    return std::strcmp(lhs.Data(), rhs.Data()) == 0;
+}
+
+const char* StringView::Data() const {
+    return begin_;
+}
+
+const char& StringView::Front() const {
+    return *begin_;
+}
+
+const char& StringView::Back() const {
+    return *(end_ - 1);
+}
+
+size_t StringView::Size() const {
+    return end_ - begin_;
+}
+
+size_t StringView::Length() const {
+    return Size();
+}
+
+bool StringView::Empty() const {
+    if (begin_ == end_ || begin_ == nullptr) {
+        return true;
+    }
+
+    return false;
+}
+
+int StringView::Compare(StringView& str) const {
+    return std::strcmp(begin_, str.begin_);
+}
+
+void StringView::RemovePrefix(size_t len) {
+    if (begin_ + len >= end_) {
+        begin_ = end_;
+        return;
+    }
+
+    begin_ += len;
+}
+
+void StringView::RemoveSuffix(size_t len) {
+    if (end_ - len <= begin_) {
+        end_ = begin_;
+        return;
+    }
+
+    end_ -= len;
+}
+
+StringView StringView::Substr(size_t start = 0, size_t len = 0) const {
+    if (start > Size() || Empty()) {
+        return StringView();
+    }
+
+    if (len == 0) {
+        return StringView(begin_ + start);
+    }
+
+    return StringView(begin_ + start, len);
+}
+
+size_t StringView::Find(char symbol, size_t start = 0) const {
+    if (start > Size() || Empty()) {
+        return npos;
+    }
+
+    const char* pos = std::strchr(begin_ + start, symbol);
+
+    if (!pos) {
+        return npos;
+    } 
+
+    return pos - begin_;
+}
+
+size_t StringView::Find(StringView str, size_t start = 0) const {
+    if (start > Size() || str.Size() > Size() || Empty()) {
+        return npos;
+    }
+
+    const char* pos = std::strstr(begin_ + start, str.begin_);
+
+    if (!pos) {
+        return npos;
+    }
+
+    return pos - begin_;
+}
+
+std::string StringView::ToString() const {
+    return std::string(begin_, Size());
+}
--- 05_week/tasks/tracer/tracer.cpp
+++ 05_week/tasks/tracer/tracer.cpp
@@ -16,51 +16,51 @@ public:
         ++count;
         ++default_ctor;
         ++alive;
-        id = count;
-        name = "obj_" + std::to_string(id);
+        id_ = count;
+        name_ = "obj_" + std::to_string(id_);
     }
 
     Tracer(const std::string& objName) {
         ++count;
         ++str_ctor;
         ++alive;
-        id = count;
-        name = objName + '_' + std::to_string(id);
+        id_ = count;
+        name_ = objName + '_' + std::to_string(id_);
     }
 
     Tracer(const Tracer& other) {
         ++count;
         ++copy_ctor;
         ++alive;
-        id = count;
-        name = other.name;
+        id_ = count;
+        name_ = other.name_;
     }
 
     Tracer(Tracer&& other) noexcept {
         ++count;
         ++move_ctor;
         ++alive;
-        id = count;
-        name = std::move(other.name);
+        id_ = count;
+        name_ = std::move(other.name_);
     }
 
     Tracer& operator=(const Tracer& other) {
-        if (other.id == id ) {
+        if (other.id_ == id_ ) {
             return *this;
         }
 
         ++copy_assign;
-        name = other.name;
+        name_ = other.name_;
         return *this;
     }
 
     Tracer& operator=(Tracer&& other) noexcept {
-        if (other.id == id) {
+        if (other.id_ == id_) {
             return *this;
         }
 
         ++move_assign;
-        name = std::move(other.name);
+        name_ = std::move(other.name_);
         return *this;
     }
 
@@ -75,8 +75,8 @@ public:
     static void ResetStats(); 
     
 private:
-    size_t id;
-    std::string name;
+    size_t id_;
+    std::string name_;
 };
 
 size_t Tracer::count = 0;
@@ -90,15 +90,15 @@ size_t Tracer::dtor = 0;
 size_t Tracer::alive = 0;
 
 size_t Tracer::Id() {
-    return id;
+    return id_;
 }
 
 const std::string& Tracer::Name() const {
-    return name;
+    return name_;
 }
 
 const char* Tracer::Data() const {
-    return name.data();
+    return name_.data();
 }
 
 void Tracer::ResetStats() {
