bef20971742953e492557a96c108dcbbecd65cf2
Nicolay Petrov <lilpip2002@mail.ru>
2026-02-08 19:01:38 +0500

    add (solution): add path 04_week task

--- 04_week/tasks/stack/stack.cpp
+++ 04_week/tasks/stack/stack.cpp
@@ -1,11 +1,85 @@
 #include <vector>
 
 
-class Stack {
+// class Stack {
+// private:
+//     std::vector<int> stack;
+// public:
+//     //Объявление методов
+//     void Push(int val);
+//     bool Pop();
+//     int& Top();
+//     const int& Top() const;
+//     bool Empty() const;
+//     size_t Size() const;
+//     void Clear();
+//     void Swap(Stack& stack2);
+//     friend bool operator==(const Stack& lhs, const Stack& rhs);
+//     friend bool operator!=(const Stack& lhs, const Stack& rhs);
+// };
+
+// //Реализация методов
+// void Stack::Push(int val){
+//     stack.push_back(val);
+// }
+
+// bool Stack::Pop(){
+//     if (stack.empty()){
+//         return false;
+//     } else{
+//         stack.pop_back();
+//         return true;
+//     }
+// }
+
+// int& Stack::Top(){
+//     return stack.back();
+// }
+// //Перегрузка Top
+// const int& Stack::Top() const{
+//     return stack.back();
+// }
+
+// bool Stack::Empty() const{
+//     return stack.empty();
+// }
+
+// size_t Stack::Size() const{
+//     return stack.size();
+// }
+
+// void Stack::Clear(){
+//     stack.clear();
+// }
+
+// void Stack::Swap(Stack& stack2){
+//     stack.swap(stack2.stack);
+// }
+
+// //Операторы
+// bool operator==(const Stack& lhs, const Stack& rhs){
+//     return lhs.stack == rhs.stack;
+// }
+
+// bool operator!=(const Stack& lhs, const Stack& rhs){
+//     return lhs.stack != rhs.stack;
+// }
+
+
+
+
+
+
+
+
+
+
+
+class Stack
+{
 private:
     std::vector<int> stack;
 public:
-    //Объявление методов
     void Push(int val);
     bool Pop();
     int& Top();
@@ -13,54 +87,51 @@ public:
     bool Empty() const;
     size_t Size() const;
     void Clear();
-    void Swap(Stack& stack2);
+    void Swap(Stack& other);
     friend bool operator==(const Stack& lhs, const Stack& rhs);
     friend bool operator!=(const Stack& lhs, const Stack& rhs);
 };
 
-//Реализация методов
-void Stack::Push(int val){
-    stack.push_back(val);
+void Stack::Push(int val) {
+    return stack.push_back(val);
 }
 
-bool Stack::Pop(){
-    if (stack.empty()){
+bool Stack::Pop() {
+    if (stack.empty()) {
         return false;
-    } else{
-        stack.pop_back();
-        return true;
     }
+    stack.pop_back();
+    return true;
 }
 
-int& Stack::Top(){
-    return stack.back();
-}
-//Перегрузка Top
-const int& Stack::Top() const{
+int& Stack::Top() {
     return stack.back();
 }
 
-bool Stack::Empty() const{
+const int& Stack::Top() const {
+    return stack.back();
+}
+
+bool Stack::Empty() const {
     return stack.empty();
 }
 
-size_t Stack::Size() const{
+size_t Stack::Size() const {
     return stack.size();
 }
 
-void Stack::Clear(){
-    stack.clear();
+void Stack::Clear() {
+    return stack.clear();
 }
 
-void Stack::Swap(Stack& stack2){
-    stack.swap(stack2.stack);
+void Stack::Swap(Stack& other) {
+    return stack.swap(other.stack);
 }
 
-//Операторы
-bool operator==(const Stack& lhs, const Stack& rhs){
+bool operator==(const Stack& lhs, const Stack& rhs) {
     return lhs.stack == rhs.stack;
 }
 
-bool operator!=(const Stack& lhs, const Stack& rhs){
-    return lhs.stack != rhs.stack;
+bool operator!=(const Stack& lhs, const Stack& rhs) {
+    return !(lhs == rhs);
 }
