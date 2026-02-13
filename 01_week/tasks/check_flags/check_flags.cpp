abc3ef0947785bfc7ed8d7142320b06bc5d2aa44
Evgenii Sh <79617771614@yandex.ru>
2026-02-13 10:53:10 +0500

    Update check_flags.cpp

--- 01_week/tasks/check_flags/check_flags.cpp
+++ 01_week/tasks/check_flags/check_flags.cpp
@@ -1,7 +1,6 @@
 #include <cstdint>
 #include <stdexcept>
 
-
 enum class CheckFlags : uint8_t {
     NONE = 0,
     TIME = (1 << 0),
@@ -14,5 +13,68 @@ enum class CheckFlags : uint8_t {
 };
 
 void PrintCheckFlags(CheckFlags flags) {
-    throw std::runtime_error{"Not implemented"};
+    
+    char buf[40] = {'\0'};
+    uint8_t data = static_cast<uint8_t>(flags); // приведение типа к uint8_t т.к. CheckFlags типа uint8_t
+    size_t i = 0;
+    
+    buf[0] = '[';
+    i =1;
+    // Проверка наличия "лишних флагов", ALL приводим к типу uint8_t
+    // Если есть лишние флаги, выходим.
+    if(data > static_cast<uint8_t>(CheckFlags::ALL))        return;
+    if(data != 0) 
+    {
+            if(data & static_cast<uint8_t>(CheckFlags::TIME)){
+                if(buf[i-1] != '['){
+                    buf[i] = ',';
+                    i++;
+                } 
+                
+                buf[i++] = 'T'; buf[i++] = 'I'; buf[i++] = 'M'; buf[i++] = 'E';
+            }
+            if(data & static_cast<uint8_t>(CheckFlags::DATE)){
+                if(buf[i-1] != '['){
+                    buf[i] = ',';
+                    i++;
+                } 
+                
+                buf[i++] = 'D'; buf[i++] = 'A'; buf[i++] = 'T'; buf[i++] = 'E';
+            } 
+            if(data & static_cast<uint8_t>(CheckFlags::USER)){
+                if(buf[i-1] != '['){
+                    buf[i] = ',';
+                    i++;
+                } 
+                
+                buf[i++] = 'U'; buf[i++] = 'S'; buf[i++] = 'E'; buf[i++] = 'R';
+            }
+            if(data & static_cast<uint8_t>(CheckFlags::CERT)){
+                if(buf[i-1] != '['){
+                    buf[i] = ',';
+                    i++;
+                } 
+                
+                buf[i++] = 'C'; buf[i++] = 'E'; buf[i++] = 'R'; buf[i++] = 'T';
+            }
+            if(data & static_cast<uint8_t>(CheckFlags::KEYS)){
+                if(buf[i-1] != '['){
+                    buf[i] = ',';
+                    i++;
+                } 
+                
+                buf[i++] = 'K'; buf[i++] = 'E'; buf[i++] = 'Y'; buf[i++] = 'S';
+            } 
+            if(data & static_cast<uint8_t>(CheckFlags::DEST)){
+                if(buf[i-1] != '['){
+                    buf[i] = ',';
+                    i++;
+                } 
+                
+                buf[i++] = 'D'; buf[i++] = 'E'; buf[i++] = 'S'; buf[i++] = 'T';
+            } 
+    }
+    
+    buf[i] = ']';
+    std::cout << buf; 
 }
