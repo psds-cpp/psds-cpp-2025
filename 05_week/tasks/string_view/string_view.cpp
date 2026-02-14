f0d41a4671747a4784f5a70b435c943ca5735b79
Eremin Daniil <danchchik@bk.ru>
2026-02-14 20:01:41 +0500

    modify (solution): Полное решение string_view

--- 05_week/tasks/string_view/string_view.cpp
+++ 05_week/tasks/string_view/string_view.cpp
@@ -5,17 +5,17 @@ class StringView {
 public:
     StringView() = default;
     StringView(const std::string& str, size_t begin, size_t len_substr);
-    StringView(const std::string&& str, size_t begin, size_t len_substr);
+    StringView(const std::string&& str) = delete;  // явно указываем что нельзя сконструировать stringview из rvalue ссылки
     StringView(const char* cstr, size_t len_substr);
     const char& operator[](size_t index) const;
-    const char* Data() const { return data_; }
-    const char& Front() const { return *(data_); }
-    const char& Back() const { return *(data_ + length_ - 1); }
-    size_t Size() const { return length_; }
-    size_t Length() const { return length_; }
-    bool Empty() const { return length_ == 0; }
-    void RemovePrefix(size_t count);
-    void RemoveSuffix(size_t count);
+    const char* Data() const noexcept { return data_; }
+    const char& Front() const noexcept { return *(data_); }
+    const char& Back() const noexcept { return *(data_ + length_ - 1); }
+    size_t Size() const noexcept { return length_; }
+    size_t Length() const noexcept { return length_; }
+    bool Empty() const noexcept { return length_ == 0; }
+    void RemovePrefix(size_t count) noexcept;
+    void RemoveSuffix(size_t count) noexcept;
     StringView Substr(size_t begin = 0, size_t len = npos) const;
     size_t Find(StringView strview, size_t pos) const;
     size_t Find(char c, size_t pos) const;
@@ -37,13 +37,6 @@ StringView::StringView(const std::string& str, size_t begin = 0, size_t len_subs
     }
 }
 
-StringView::StringView(const std::string&& str, size_t begin = 0, size_t len_substr = npos) {
-    if(begin <= str.size()){
-        data_ = str.c_str() + begin;
-        length_ = std::min(str.size() - begin, len_substr);
-    }
-}
-
 StringView::StringView(const char* cstr, size_t len_substr = npos) {
     if(cstr && len_substr > 0) {
         data_ = cstr;
@@ -58,6 +51,7 @@ const char& StringView::operator[](size_t index) const {
 StringView StringView::Substr(size_t begin, size_t len) const {
     return begin < length_ ? StringView(data_ + begin, len) : StringView(data_, 0);
 }
+
 size_t StringView::Find(StringView strview, size_t pos = 0) const {
     pos = std::min(pos, length_);
     if (Empty() || strview.Size() > length_ - pos) {
@@ -80,7 +74,7 @@ size_t StringView::Find(char c, size_t pos = 0) const {
     }
 
     pos = std::min(pos, length_);
-    for (size_t i = pos; i <= length_; ++i) {
+    for (size_t i = pos; i < length_; ++i) {
         if (*(data_ + i) == c) {
             return i;
         }
@@ -113,11 +107,11 @@ bool operator!=(StringView lhs, StringView rhs){
     return !(lhs == rhs);
 }
 
-void StringView::RemovePrefix(size_t count) {
+void StringView::RemovePrefix(size_t count) noexcept {
     data_ += count > length_ ? length_ : count;
     length_ -= count > length_ ? length_ : count;
 }
 
-void StringView::RemoveSuffix(size_t count) {
+void StringView::RemoveSuffix(size_t count) noexcept {
     length_ -= count > length_ ? length_ : count;
 }
